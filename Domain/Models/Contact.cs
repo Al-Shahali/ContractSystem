781c88d5b82cee82b525c0328d86e374ddc40e1d
Ahmed Al Shahali <inback101@gmail.com>
2024-02-26 18:27:34 +0200

    Add project files.

---
+++ Domain/Models/Contact.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Models;
+
+public partial class Contact
+{
+    /// <summary>
+    /// Contact Code
+    /// </summary>
+    public int Concod { get; set; }
+
+    /// <summary>
+    /// Contact Name
+    /// </summary>
+    public string Name { get; set; } = null!;
+
+    /// <summary>
+    /// Contact Phone
+    /// </summary>
+    public string Phone { get; set; } = null!;
+
+    /// <summary>
+    /// Contact Address
+    /// </summary>
+    public string Address { get; set; } = null!;
+
+    /// <summary>
+    /// Contact Note
+    /// </summary>
+    public string? Note { get; set; }
+}
